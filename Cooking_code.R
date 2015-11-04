860f7f05588189d8b9cfffe695bdf8efde8f9d2b
rishabh1990 <verma.rishabh1990@gmail.com>
2015-11-03 21:21:43 -0600

    Update Cooking_code.R

--- Cooking_code.R
+++ Cooking_code.R
@@ -2,7 +2,6 @@ library("jsonlite")
 library(dplyr)
 cook<- fromJSON("train.json")
 View(cook)
-library(dplyr)
 cuisine_freq<- cook %>% group_by(cuisine) %>% summarize(count=n())
 library(ggplot2)
 plot<- ggplot(cuisine_freq, aes(cuisine, count)) + geom_point() + theme_bw()
@@ -15,5 +14,18 @@ final_ingredients<- t(final_ingredients)
 cuisine<- as.data.frame(cuisine=cook$cuisine)
 final_cook<- cbind(final_ingredients, cuisine)
 write.csv(final_cook, file="cooking.csv")
-library(rattle)
-rattle()
+#Partitioning the Dataset into training and validation
+library("caret")
+intrain<- createDataPartition(cook$cook.cuisine, p=0.7, list=FALSE)
+training<- cook[intrain,]
+validation<- cook[-intrain,]
+#Building the decision tree model
+library(rpart)
+library(rpart.plot)
+model<- rpart(cook.cuisine~.,data=training,method="class")
+prp(model)
+pred_model<- predict(model, newdata=validation, type="class")
+head(pred_model,1)
+CM<- confusionMatrix(pred_model, validation$cook.cuisine)
+CM
+
